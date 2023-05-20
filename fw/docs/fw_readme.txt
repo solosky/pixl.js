62b7d7e4bb91d97f2741d3fe1613c92ee7c13a54
solosky <solosky772@qq.com>
2023-05-20 16:58:27 +0800

    add fw update script

---
+++ fw/docs/fw_readme.txt
@@ -0,0 +1,12 @@
+如何烧写固件：
+
+方法一：线刷
+此方法需要额外购买CMASS-DAP兼容的下载器，推荐9.9元的pwlink2 lite
+购买地址：https://item.taobao.com/item.htm?spm=a1z09.2.0.0.4b942e8deXyaQO&id=675067753017&_u=d2p75qfn774a
+
+连接好线后双击fw_update.bat烧写固件。
+
+方法二：OTA更新
+此方法仅适用于已经通过线刷成功的Pixl.js设备。
+安装nrfconnect应用（iOS、Android都支持），设备在 设置 菜单中选择 ”固件更新“ 菜单后，设备会进入DFU模式，然后使用nrfconnect连接名为pixl dfu设备更新固件。
+固件是压缩包里面的pixjs_ota_vxxx.zip，需要通过微信或者QQ分享给nrfconnect应用。
---
+++ fw/scripts/fw_update.bat
@@ -0,0 +1,59 @@
+@rem 这个脚本可以用来一键烧写固件 
+
+@echo off
+
+echo ------------------------------------------------------------------------------
+echo Pixl.js 固件自动烧写脚本 @solosky
+echo ------------------------------------------------------------------------------
+
+set openocd_win64_download_url="https://ghproxy.com/https://github.com/openocd-org/openocd/releases/download/v0.12.0/openocd-v0.12.0-i686-w64-mingw32.tar.gz"
+set openocd_win64_file="openocd-v0.12.0-i686-w64-mingw32.tar.gz"
+set wget_win64_download_url="https://eternallybored.org/misc/wget/1.21.4/64/wget.exe"
+set openocd_bin="./bin/openocd.exe"
+
+if not exist %openocd_bin% (
+    echo 下载wget...
+    curl -o wget.exe %wget_win64_download_url%
+    if not  %errorlevel%==0 (
+        echo "wget 下载失败，请重试！"
+        exit -1
+    )
+
+    echo 下载openocd.. 
+    wget.exe -O %openocd_win64_file% %openocd_win64_download_url%
+    if not  %errorlevel%==0 (
+        echo "openocd 下载失败，请重试！"
+        exit -1
+    )
+
+    echo 解压缩openocd..
+    tar -xzf %openocd_win64_file%
+) else (
+    echo openocd已经安装，跳过下载
+)
+
+echo 检查openocd版本
+bin\openocd -v
+
+echo 刷新固件中，请稍后...
+openocd -f interface/cmsis-dap.cfg -f target/nrf52.cfg -c init -c "reset init" -c halt -c "nrf51 mass_erase" -c "program pixljs_all.hex verify"  -c reset -c exit
+if %errorlevel%==0 (
+    echo -----------------------------------------------------------------------------
+    echo 恭喜，刷新固件成功！
+    echo -----------------------------------------------------------------------------
+) else (
+    echo -----------------------------------------------------------------------------
+    echo 刷新固件失败，请查看输出中的内容判断可能的错误原因，其中常见的错误原因如下：
+    echo -----------------------------------------------------------------------------
+    echo 1. 如果最后一行输出 "Error: Error connecting DP: cannot read IDR": 未能检测到芯片
+    echo ----a. 检查nrf52832芯片是否焊接正常
+    echo ----b. 检查下载器接线是否正确
+    echo 2. 如果最后一行输出 "Error: unable to find a matching CMSIS-DAP device": 未能检测到下载器
+    echo ----a. 检查下载器是否正确插入和识别为CMISS-DAP设备
+    echo 3. 如果最后一行输出 "Error: error writing to flash at address 0x00000000 at offset 0x00000000": 烧写失败
+    echo ----a. 重试烧写即可
+    echo 其他原因请加QQ群 109761876 询问解决
+    
+)
+
+pause
