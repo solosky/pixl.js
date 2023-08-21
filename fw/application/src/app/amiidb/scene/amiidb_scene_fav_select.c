fc3e7e9ad4972b9cfb704b018159e384f8603de6
solosky <solosky772@qq.com>
2023-08-21 23:59:51 +0800

    add new menu for fav select

--- fw/application/src/app/amiidb/scene/amiidb_scene_fav_select.c
+++ fw/application/src/app/amiidb/scene/amiidb_scene_fav_select.c
@@ -6,10 +6,10 @@
 
 #include "amiidb_api_fav.h"
 #include "db_header.h"
+#include "i18n/language.h"
 #include "mui_icons.h"
 #include "settings.h"
 #include "vfs.h"
-#include "i18n/language.h"
 #include <math.h>
 
 static void amiidb_scene_fav_select_reload(app_amiidb_t *app);
@@ -29,6 +29,19 @@ static void amiidb_scene_fav_select_msg_show(app_amiidb_t *app, const char *mess
     mui_view_dispatcher_switch_to_view(app->p_view_dispatcher, AMIIDB_VIEW_ID_MSG_BOX);
 }
 
+static void amiidb_scene_fav_select_text_input_event_cb(mui_text_input_event_t event, mui_text_input_t *p_text_input) {
+    app_amiidb_t *app = p_text_input->user_data;
+    if (event == MUI_TEXT_INPUT_EVENT_CONFIRMED) {
+        const char *text = mui_text_input_get_input_text(p_text_input);
+        if (strlen(text) > 0) {
+            int32_t res = amiidb_api_fav_create_dir(text);
+            if (res == VFS_OK) {
+                amiidb_scene_fav_select_reload(app);
+            }
+        }
+    }
+}
+
 static void amiidb_scene_fav_select_list_view_on_selected(mui_list_view_event_t event, mui_list_view_t *p_list_view,
                                                           mui_list_item_t *p_item) {
     uint16_t icon = p_item->icon;
@@ -38,6 +51,12 @@ static void amiidb_scene_fav_select_list_view_on_selected(mui_list_view_event_t 
     case ICON_EXIT:
         mui_scene_dispatcher_previous_scene(app->p_scene_dispatcher);
         break;
+
+    case ICON_NEW: {
+        mui_text_input_set_header(app->p_text_input, getLangString(_L_APP_AMIIDB_FAV_NEW_HEAD));
+        mui_text_input_set_event_cb(app->p_text_input, amiidb_scene_fav_select_text_input_event_cb);
+        mui_view_dispatcher_switch_to_view(app->p_view_dispatcher, AMIIDB_VIEW_ID_INPUT);
+    } break;
 
     case ICON_FOLDER: {
         uint8_t cur_game_id = app->game_id_path[app->game_id_index];
@@ -64,13 +83,15 @@ static void amiidb_scene_fav_select_reload(app_amiidb_t *app) {
     // clear list view
     mui_list_view_clear_items(app->p_list_view);
 
-    mui_list_view_add_item(app->p_list_view, ICON_FAVORITE, getLangString(_L_APP_AMIIDB_FAV_SELECT_FOLDER), (void *)0);
+    mui_list_view_add_item(app->p_list_view, ICON_NEW, getLangString(_L_APP_AMIIDB_NEW), (void *)0);
 
     amiidb_api_fav_list_dir("", amiidb_scene_fav_select_read_cb, app);
 
     mui_list_view_add_item(app->p_list_view, ICON_EXIT, getLangString(_L_APP_AMIIDB_BACK), (void *)0);
     mui_list_view_set_selected_cb(app->p_list_view, amiidb_scene_fav_select_list_view_on_selected);
     mui_list_view_set_user_data(app->p_list_view, app);
+
+    // no fav folder, first create one
     mui_view_dispatcher_switch_to_view(app->p_view_dispatcher, AMIIDB_VIEW_ID_LIST);
 }
 
