a06cb4b0e93499bc2c3fecfe426315749e5cd736
solosky <solosky772@qq.com>
2023-10-20 21:28:59 +0800

    auto wrap for amiibo name

--- fw/application/src/app/amiibo/view/amiibo_detail_view.c
+++ fw/application/src/app/amiibo/view/amiibo_detail_view.c
@@ -44,7 +44,7 @@ static void amiibo_detail_view_on_draw(mui_view_t *p_view, mui_canvas_t *p_canva
     const db_amiibo_t *amd = get_amiibo_by_id(head, tail);
     if (amd != NULL) {
         const char *name = getLanguage() == LANGUAGE_EN_US ? amd->name_en : amd->name_cn;
-        mui_canvas_draw_utf8(p_canvas, 0, y += 13, name);
+        mui_element_autowrap_text(p_canvas, 0, y += 15, mui_canvas_get_width(p_canvas), 24, name);
         if (strlen(ntag->notes) > 0) {
             mui_element_autowrap_text(p_canvas, 0, y += 13, mui_canvas_get_width(p_canvas), 24, ntag->notes);
         }
