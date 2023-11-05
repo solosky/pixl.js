dc25c1b0df7c4553010c685cd5995b46017b8332
impeeza <41979604+impeeza@users.noreply.github.com>
2023-11-05 16:21:54 -0500

    Update index.js
    remove unneded browser language select.  if no cookie for lang, return "zn_TW" :)

--- web/src/i18n/index.js
+++ web/src/i18n/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
+import cookies from "vue-cookie"
 import elementEnLocale from 'element-ui/lib/locale/lang/en' // element-ui lang
 import elementZhCnLocale from 'element-ui/lib/locale/lang/zh-CN'// element-ui lang
 import elementEsLocale from 'element-ui/lib/locale/lang/es'// element-ui lang
@@ -31,15 +32,10 @@ const messages = {
   }
 }
 export function getLanguage() {
+  //read actual cookie for language
+  const chooseLanguage = cookies.get('lang')
+  if (chooseLanguage) return chooseLanguage
   // if has not choose language
-  const language = (navigator.language || navigator.browserLanguage).toLowerCase()
-  const locales = Object.keys(messages)
-  for (const locale of locales) {
-    if (language.indexOf(locale) > -1) {
-      //return 'en'
-      return locale
-    }
-  }
   return 'zh_TW'
 }
 const i18n = new VueI18n({
