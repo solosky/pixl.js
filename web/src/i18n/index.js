fa7b2d6fdaf6dfd40fd1b78d957d02a6c2da3b63
impeeza <41979604+impeeza@users.noreply.github.com>
2023-11-04 23:30:16 -0500

    Web Site Internationalization
    Changes and additions needed to add Internationalization to Pixl.js Website.

---
+++ web/src/i18n/index.js
@@ -0,0 +1,53 @@
+import Vue from 'vue'
+import VueI18n from 'vue-i18n'
+import elementEnLocale from 'element-ui/lib/locale/lang/en' // element-ui lang
+import elementZhCnLocale from 'element-ui/lib/locale/lang/zh-CN'// element-ui lang
+import elementEsLocale from 'element-ui/lib/locale/lang/es'// element-ui lang
+import elementZhTwLocale from 'element-ui/lib/locale/lang/zh-TW'// element-ui lang
+import enLocale from './en_US'
+import zhTWLocale from './zh_TW'
+import zhCNLocale from './zh_Hans'
+import esLocale from './es_ES'
+
+
+Vue.use(VueI18n)
+
+const messages = {
+  en: {
+    ...enLocale,
+    ...elementEnLocale
+  },
+  zh_TW: {
+    ...zhTWLocale,
+    ...elementZhTwLocale
+  },
+  es: {
+    ...esLocale,
+    ...elementEsLocale
+  },
+  zh_CN: {
+    ...zhCNLocale,
+    ...elementZhCnLocale
+  }
+}
+export function getLanguage() {
+  // if has not choose language
+  const language = (navigator.language || navigator.browserLanguage).toLowerCase()
+  const locales = Object.keys(messages)
+  for (const locale of locales) {
+    if (language.indexOf(locale) > -1) {
+      //return 'en'
+      return locale
+    }
+  }
+  return 'zh_TW'
+}
+const i18n = new VueI18n({
+  // set locale
+  // options: en | zh | es
+  locale: getLanguage(),
+  // set locale messages
+  messages
+})
+
+export default i18n
--- web/src/main.js
+++ web/src/main.js
@@ -3,9 +3,14 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 
-Vue.use(ElementUI)
+import i18n from './i18n' // internationalization
+
+Vue.use(ElementUI, {
+  i18n: (key, value) => i18n.t(key, value)
+})
 
 new Vue({
   el: '#app',
+  i18n,
   render: h => h(App)
 })
