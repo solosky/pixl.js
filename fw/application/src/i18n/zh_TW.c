16d0a10329706f609a76bccebc1fc78dacf77e9f
solosky <solosky772@qq.com>
2023-11-19 16:50:00 +0800

    manual merge PR #144

--- fw/application/src/i18n/zh_TW.c
+++ fw/application/src/i18n/zh_TW.c
@@ -81,7 +81,7 @@ const char * lang_zh_TW[_L_COUNT] = {
     [_L_MODE] = "模式",
     [_L_AUTO_RANDOM] = "自動隨機",
     [_L_COMPATIBLE_MODE] = "相容模式",
-    [_L_TAG_DETAILS] = "標籤詳情",
+    [_L_TAG_DETAILS] = "[標籤詳情]",
     [_L_MAIN_MENU] = "[主選單]",
     [_L_MODE_RANDOM] = "隨機(手動)",
     [_L_MODE_CYCLE] = "按序",
@@ -122,4 +122,5 @@ const char * lang_zh_TW[_L_COUNT] = {
     [_L_APP_AMIIDB_FAV_FAILED] = "收藏失敗",
     [_L_APP_AMIIDB_SLOT_SAVE_SUCCESS] = "保存成功",
     [_L_APP_AMIIDB_SLOT_SAVE_FAILED] = "保存失敗",
+    [_L_APP_CHAMELEON] = "卡模拟器",
 };
