7ec8d572518c7903e437cb581ef55a0cb2bb8375
Stefano Navarria <stefanonavarria@Stefanos-Mac-Pro.local>
2024-03-30 09:30:54 +0100

    changed it_IT language index

--- fw/application/src/i18n/language.h
+++ fw/application/src/i18n/language.h
@@ -30,7 +30,6 @@ extern const char* lang_en_US[_L_COUNT];
 extern const char* lang_zh_Hans[_L_COUNT];
 extern const char* lang_zh_TW[_L_COUNT];
 extern const char* lang_es_ES[_L_COUNT];
-extern const char* lang_it_IT[_L_COUNT];
 extern const char* lang_hu_HU[_L_COUNT];
 extern const char* lang_de_DE[_L_COUNT];
 extern const char* lang_fr_FR[_L_COUNT];
@@ -38,7 +37,7 @@ extern const char* lang_nl_NL[_L_COUNT];
 extern const char* lang_pt_BR[_L_COUNT];
 extern const char* lang_ja_JP[_L_COUNT];
 extern const char* lang_pt_PT[_L_COUNT];
-
+extern const char* lang_it_IT[_L_COUNT];
 
 // 获取字符串的函数 (Get language string function)
 const char* getLangString(L_StringID stringID);
