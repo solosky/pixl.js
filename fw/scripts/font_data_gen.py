e15a7c1ebe517246913727f14f9b874a8cac7c59
Schoko <Schokobecher@gmail.com>
2024-04-05 15:46:45 +0700

    Minor formatting fixes

--- fw/scripts/font_data_gen.py
+++ fw/scripts/font_data_gen.py
@@ -10,7 +10,7 @@ import platform
 current_dir = os.path.dirname(os.path.abspath(__file__))
 source_dirs = [
     os.path.join(current_dir, "../application/src/i18n"),
-    os.path.join(current_dir, "../application/src/amiidb")
+    os.path.join(current_dir, "../application/src/amiidb"),
 ]
 
 # Absolute path to the directory where the output file will be saved
@@ -22,7 +22,7 @@ output_file = "pixjs.txt"
 
 
 def write_to_file(file_path, content):
-    with open(file_path, 'w', encoding='utf-8') as f:
+    with open(file_path, "w", encoding="utf-8") as f:
         f.write(content)
 
 
@@ -34,24 +34,28 @@ def extract_non_printable_chars():
                 if file.endswith(".c"):
                     with open(os.path.join(root, file), "r", encoding="utf-8") as f:
                         content = f.read()
-                        non_printable_chars.update(re.findall(r'[^\x20-\x7E]', content))
+                        non_printable_chars.update(re.findall(r"[^\x20-\x7E]", content))
 
-    write_to_file(os.path.join(data_dir, output_file), '\n'.join(sorted(non_printable_chars)))
+    write_to_file(
+        os.path.join(data_dir, output_file), "\n".join(sorted(non_printable_chars))
+    )
 
 
 def convert_and_sort(unicode_strings):
-    return sorted(set(f'${ord(char):04X},' for string in unicode_strings for char in string))
+    return sorted(
+        set(f"${ord(char):04X}," for string in unicode_strings for char in string)
+    )
 
 
 def run_bdfconv(map_path, output_path, bdf_path):
     # Construct the path to the bdfconv executable relative to the script directory
     system = platform.system()
-    if system == 'Windows':
-        bdfconv_path = os.path.join(current_dir, 'bdfconv.exe')
-    elif system == 'Darwin':
-        bdfconv_path = os.path.join(current_dir, 'bdfconv_macos_universal')
-    elif system == 'Linux':
-        bdfconv_path = os.path.join(current_dir, 'bdfconv_linux')
+    if system == "Windows":
+        bdfconv_path = os.path.join(current_dir, "bdfconv.exe")
+    elif system == "Darwin":
+        bdfconv_path = os.path.join(current_dir, "bdfconv_macos_universal")
+    elif system == "Linux":
+        bdfconv_path = os.path.join(current_dir, "bdfconv_linux")
     else:
         raise OSError("Unsupported operating system")
 
@@ -61,8 +65,18 @@ def run_bdfconv(map_path, output_path, bdf_path):
     abs_bdf_path = os.path.abspath(bdf_path)
 
     cmd = [
-        bdfconv_path, '-b', '0', '-f', '1', '-M', abs_map_path, 
-        '-n', 'u8g2_font_wqy12_t_gb2312a', '-o', abs_output_path, abs_bdf_path
+        bdfconv_path,
+        "-b",
+        "0",
+        "-f",
+        "1",
+        "-M",
+        abs_map_path,
+        "-n",
+        "u8g2_font_wqy12_t_gb2312a",
+        "-o",
+        abs_output_path,
+        abs_bdf_path,
     ]
     subprocess.run(cmd, check=True)
 
@@ -70,38 +84,44 @@ def run_bdfconv(map_path, output_path, bdf_path):
 def main():
     extract_non_printable_chars()
 
-    combined_content = ''
-    for file_name in ['chinese3.txt', 'pixjs.txt']:
-        with open(os.path.join(data_dir, file_name), 'r', encoding='utf-8') as file:
+    combined_content = ""
+    for file_name in ["chinese3.txt", "pixjs.txt"]:
+        with open(os.path.join(data_dir, file_name), "r", encoding="utf-8") as file:
             combined_content += file.read()
 
     sorted_converted = convert_and_sort(combined_content.splitlines())
-    write_to_file(os.path.join(data_dir, 'gb2312a.map'), '32-128,\n' + '\n'.join(sorted_converted))
+    write_to_file(
+        os.path.join(data_dir, "gb2312a.map"), "32-128,\n" + "\n".join(sorted_converted)
+    )
 
     run_bdfconv(
-        map_path=os.path.join(data_dir, 'gb2312a.map'),
-        output_path=os.path.join(mui_dir, 'u8g2_font_wqy12_t_gb2312a_t.c'),
-        bdf_path=os.path.join(data_dir, 'wenquanyi_9pt_u8g2.bdf')
+        map_path=os.path.join(data_dir, "gb2312a.map"),
+        output_path=os.path.join(mui_dir, "u8g2_font_wqy12_t_gb2312a_t.c"),
+        bdf_path=os.path.join(data_dir, "wenquanyi_9pt_u8g2.bdf"),
     )
 
     # Write to u8g2_font_wqy12_t_gb2312a.c
-    with open(os.path.join(mui_dir, 'u8g2_font_wqy12_t_gb2312a.c'), 'w+', encoding='utf-8') as final_file:
-        final_file.write('''
+    with open(
+        os.path.join(mui_dir, "u8g2_font_wqy12_t_gb2312a.c"), "w+", encoding="utf-8"
+    ) as final_file:
+        final_file.write(
+            """
 #include "mui_u8g2.h"
 
 #include "u8x8.h"
 
-''')
+"""
+        )
 
         # Read the content of the temporary file and append it to the final file
-        temp_file_path = os.path.join(mui_dir, 'u8g2_font_wqy12_t_gb2312a_t.c')
-        with open(temp_file_path, 'r', encoding='utf-8') as temp_file:
+        temp_file_path = os.path.join(mui_dir, "u8g2_font_wqy12_t_gb2312a_t.c")
+        with open(temp_file_path, "r", encoding="utf-8") as temp_file:
             final_file.write(temp_file.read())
 
     # Remove the temporary files
     os.remove(temp_file_path)
     os.remove(os.path.join(data_dir, output_file))
-    os.remove(os.path.join(data_dir, 'gb2312a.map'))
+    os.remove(os.path.join(data_dir, "gb2312a.map"))
 
 
 if __name__ == "__main__":
