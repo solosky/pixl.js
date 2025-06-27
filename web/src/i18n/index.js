b8635a34d69d57181fb1723c5155e8533b7d9149
Giovanni Palusa <giovanni@palusa.se>
2025-06-27 11:09:32 +0200

    Added swedish web

--- web/src/i18n/index.js
+++ web/src/i18n/index.js
@@ -6,12 +6,14 @@ import elementEsLocale from 'element-ui/lib/locale/lang/es' // element-ui lang
 import elementZhTwLocale from 'element-ui/lib/locale/lang/zh-TW' // element-ui lang
 import elementRuLocale from 'element-ui/lib/locale/lang/ru-RU' // element-ui lang
 import elementDeLocale from 'element-ui/lib/locale/lang/de' // element-ui lang
+import elementSvLocale from 'element-ui/lib/locale/lang/sv' // element-ui lang
 import enLocale from './en_US'
 import zhTWLocale from './zh_TW'
 import zhCNLocale from './zh_Hans'
 import esLocale from './es_ES'
 import ruLocale from './ru_RU'
 import deLocale from './de_DE'
+import svLocale from './sv_SE'
 
 
 Vue.use(VueI18n)
@@ -40,6 +42,10 @@ const messages = {
   de_DE: {
     ... deLocale,
     ... elementDeLocale
+  },
+  sv_SE: {
+    ... svLocale
+    ... elementSvLocale
   }
 }
 export function getLanguage() {
